a3ce990246979fb1eb1dae838ddc370586d89c1d
Kelvin Zhang <zhangkelvin@google.com>
2024-05-28 09:19:54 -0700

    rosy: Allow to access bootable/deprecated-ota targets
    RM6785 is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to RM6785-common namespace list.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- device.mk
+++ device.mk
@@ -422,6 +422,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.rosy
 
+# Update
+PRODUCT_SOONG_NAMESPACES += bootable/deprecated-ota
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic
