75ec2a617d4033691e09fe1b29baaea015c29937
Yumi Yukimura <me.cafebabe@gmail.com>
2024-11-17 11:19:24 +0100

    rosy: Switch to QTI Memtrack AIDL HAL
    Change-Id: I151d5f9ca7a0e8451b3aa4fa9c7a0db411894287
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- device.mk
+++ device.mk
@@ -180,15 +180,13 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     gralloc.msm8953 \
     hwcomposer.msm8953 \
-    memtrack.msm8953 \
     libqdMetaData \
     libtinyxml \
     vendor.display.config@1.0.vendor \
-    vendor.display.config@2.0.vendor
+    vendor.display.config@2.0.vendor \
+    vendor.qti.hardware.memtrack-service
 
 # DRM
 PRODUCT_PACKAGES += \
