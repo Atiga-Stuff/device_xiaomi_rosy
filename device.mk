4535b8109b06fa3cd3d8342ea890e67920e224e5
Aditya Eka Pranata <aepranata@gmail.com>
2024-11-23 06:45:21 +0700

    rosy: Optionally build `AtigaArtsStub`
    Change-Id: Ibec9f9cbde0fac0cad2bdc31301a9e17b6baebce
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- device.mk
+++ device.mk
@@ -456,6 +456,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
 
+# Wallpaper
+ifneq ($(INCLUDE_ATIGA_ARTS),)
+PRODUCT_PACKAGES += \
+    AtigaArtsStub
+endif
+
 # Whitelisted app
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
