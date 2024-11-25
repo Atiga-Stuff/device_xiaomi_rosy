a496e83f2f95cf71a10fe080250023ff886ccba6
Aditya Eka Pranata <aepranata@gmail.com>
2024-11-25 16:01:38 +0700

    rosy: Build `libui_shim`
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- device.mk
+++ device.mk
@@ -156,7 +156,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     camera.msm8953 \
     libshimcam \
-    libgui_vendor:32
+    libgui_vendor:32 \
+		libui_shim
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
