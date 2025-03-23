9f65a90743b8cf46d0835b849e2e4a8e25dbc1b7
Aditya Eka Pranata <aepranata@gmail.com>
2025-03-23 17:42:05 +0700

    rosy: Define small and big core groups for AxionOS
    Change-Id: Ia8f81ffe45b19d43da1ed10617e2e05aef81f994
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- lineage_rosy.mk
+++ lineage_rosy.mk
@@ -23,6 +23,8 @@ AXION_CAMERA_REAR_INFO := 12
 AXION_CAMERA_FRONT_INFO := 5
 AXION_MAINTAINER := aepranata
 AXION_PROCESSOR := Snapdragon_450
+AXION_CPU_BIG_CORES := -
+AXION_CPU_SMALL_CORES := 0,1,2,3,4,5,6,7
 
 # Inherit from rosy device
 $(call inherit-product, device/xiaomi/rosy/full_rosy.mk)
