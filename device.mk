a5863eb5c6571ef378a1b04c3f158b1bc6f4e894
Aditya Eka Pranata <aepranata@gmail.com>
2025-03-30 06:04:46 +0700

    rosy:  Fix path boot-image-profile
    After commit LineageOS/android_frameworks_base@c34748c
    boot-image-profile.txt was moved to boot.
    
    Change-Id: I1b0e1024748493aa648d8810254fc67423b3b26f
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ TARGET_BOARD_SUFFIX := _64
 # Use a profile based boot image for this device. Note that this is currently a
 # generic profile and not Android Go optimized.
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
